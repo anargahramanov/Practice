ac303865a45b860122177cfff90bb57192a4d645
Anar Qəhramanov <anarg@forex-az.local>
2018-03-13 18:45:40 +0400

    changes

--- 25.02.2018 - JS/js/script.js
+++ 25.02.2018 - JS/js/script.js
@@ -1,1 +1,21 @@
-console.log("Anar");
+var image = document.querySelectorAll(".images .image");
+var all = document.querySelector(".all");
+var bigImage = document.querySelector(".all .bigImage img");
+var close = document.querySelector(".all .bigImage .close");
+
+for (var i = 0; i < image.length; i++) {
+    image[i].addEventListener("click", function () {
+        var src = this.getAttribute("title");
+        bigImage.src = src;
+        // all.style.display = "block";
+        all.classList.add("show");
+        all.classList.remove("hide");
+    })
+
+}
+
+close.addEventListener("click", function () {
+    // all.style.display = "none";
+    all.classList.add("hide");
+    all.classList.remove("show");
+});
