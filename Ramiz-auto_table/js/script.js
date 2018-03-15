ae16e782e0f8bd43c28f73d6dd49ff53fd9a5306
Anar Qəhramanov <anarg@forex-az.local>
2018-03-15 12:39:49 +0400

    Ramiz's task finished

--- Ramiz-auto_table/js/script.js
+++ Ramiz-auto_table/js/script.js
@@ -1,7 +1,22 @@
 var button = document.getElementById("button");
+var SetColor = document.getElementById("setColor");
 var col = document.getElementById("col");
 var raw = document.getElementById("raw");
 var table = document.getElementById("table");
+
+
+setColor.addEventListener("click", function () {
+    var tdS = document.querySelectorAll("tr td");
+    setInterval(function () {
+        for (var i = 0; i < tdS.length; i++) {
+            var number1 = Math.ceil(Math.random() * 256);
+            var number2 = Math.ceil(Math.random() * 256);
+            var number3 = Math.ceil(Math.random() * 256);
+            var color = "rgb(" + number1 + "," + number2 + "," + number3 + ")";
+            tdS[i].style.background = color;
+        }
+    },500);
+});
 
 
 button.addEventListener("click", function () {
@@ -10,17 +25,22 @@ button.addEventListener("click", function () {
     raw.value = "";
     col.value = "";
 
-    for (var i = 0; i < countCol; i++){
-        var td = document.createElement("td");
-        colS = td;
-    }
-
     for (var i = 0; i < countRaw; i++) {
         var tr = document.createElement("tr");
         table.appendChild(tr);
+
+        for (var j = 0; j < countCol; j++) {
+            var td = document.createElement("td");
+            tr.appendChild(td);
+            td.innerText = "Value";
+        }
     }
 
-    tr.appendChild(colS);
-    
 });
+
+
+
+
+
+
 
