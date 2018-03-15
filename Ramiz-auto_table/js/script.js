f334d72399e02becf093d97db1b42b2c36bb301d
Anar Qəhramanov <anarg@forex-az.local>
2018-03-15 14:05:45 +0400

    Ramiz's task finished

--- Ramiz-auto_table/js/script.js
+++ Ramiz-auto_table/js/script.js
@@ -1,5 +1,6 @@
 var button = document.getElementById("button");
 var SetColor = document.getElementById("setColor");
+var randomRC = document.getElementById("randomRC");
 var col = document.getElementById("col");
 var raw = document.getElementById("raw");
 var table = document.getElementById("table");
@@ -18,6 +19,27 @@ setColor.addEventListener("click", function () {
     },500);
 });
 
+randomRC.addEventListener("click",function(){
+    var numRandom1 = Math.ceil(Math.random() * 10);
+    var numRandom2 = Math.ceil(Math.random() * 10);
+
+    var countRaw = numRandom1;
+    var countCol = numRandom2;
+    raw.value = "";
+    col.value = "";
+
+    for (var i = 0; i < countRaw; i++) {
+        var tr = document.createElement("tr");
+        table.appendChild(tr);
+
+        for (var j = 0; j < countCol; j++) {
+            var td = document.createElement("td");
+            tr.appendChild(td);
+            td.innerText = "Value";
+        }
+    }
+
+});
 
 button.addEventListener("click", function () {
     var countRaw = raw.value;
